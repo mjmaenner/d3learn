8853acfba87e5c68bf2be16233f01a15f50b25e2
Matthew Maenner <maenner@gmail.com>
2015-08-07 12:31:44 -0400

    makes the file

--- school_start.R
+++ school_start.R
@@ -5,8 +5,7 @@ library(rvest)
 #install.packages("selectr")
 library(selectr)
 library(XML)
-#install.packages("maps")
-library(maps)
+library(stringr)
 
 mmwr_contents <- htmlParse("http://www.cdc.gov/mmwr/preview/mmwrhtml/mm6430a1.htm?s_cid=mm6430a1_e")
 p1<-readHTMLTable(mmwr_contents, header=TRUE, which=1)
@@ -16,19 +15,7 @@ p2<-p2[-(1:3),]
 p2<-p2[-25,]
 school<-rbind(p1,p2)
 
-school$state<-tolower(school$V1)
-
-library(stringr)
-statenames<-function(states){
-  sub_state<-character(length(states))
-  end_pos <- str_locate(states, pattern=":")
-  for (i in 1:length(states)){
-      end_pos[i]<-ifelse(is.na(end_pos[i]), nchar(states[i]), end_pos[i]-1)
-      sub_state[i]<-str_sub(states[i], start=1, end=end_pos[i])
-  }
-    return(sub_state)
-}
-
-state.fips$statename<-statenames(as.character(state.fips$polyname))
-state.fips.u<-state.fips[,-which(names(state.fips) %in% c("polyname"))]
-state.fips.u<-state.fips.u[!duplicated(state.fips),]
+names(school)[c(1,6)]<-c("state","start_time")
+school<-school[,c("state","start_time")]
+school$start_time<-ifelse(str_detect(as.character(school$start_time), "\\*"), "", as.character(school$start_time))
+write.csv(x = school, "c:/proj/misc/school.csv",row.names=FALSE)
